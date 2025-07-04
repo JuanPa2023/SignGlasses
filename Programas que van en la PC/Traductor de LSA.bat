2662cfbcda13eec3adb0b1d6a0c168b6a96e93af
JuanPa2023 <118637832+SaracinoJuanPablo@users.noreply.github.com>
2025-07-04 00:13:33 -0300

    cambios

---
+++ Programas que van en la PC/Traductor de LSA.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "D:\Proyecto Final\PROGRAMA\GITHUB\Mechatronics-Final-Proyect--4\Proyecto\me_15_freno_motor"
+jupyter nbconvert --to notebook --execute me_15_v6_eval_menu_interactivo.ipynb --inplace
+pause
